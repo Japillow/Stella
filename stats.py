cc6fc74fcb874d8cb0060801f02f86c821c4b38d
Japillow <james@barrios.me>
2019-11-25 20:15:41 +0100

    add stats documentation

--- stats.py
+++ stats.py
@@ -2,6 +2,8 @@ from collections import deque
 
 
 class Stats(object):
+    """Class used to store metrics along with related stats."""
+
     def __init__(self, check_interval, compute_timeframe):
         """Returns an instance of Stats.
 
@@ -65,52 +67,58 @@ class Stats(object):
 
     def update(self, is_up, response_time=None, response_code=None, always_a_response_code=False):
         """Updates the stats object with data from a new check.
-        
+
         Adds the success status, response time and response time to memory,
         removes old values (those older than the compute timeframe), and
-        updates the stats in constant time most of the time (apart when 
-        max or min is removed. [...])
+        updates the stats in constant time most of the time
+        (apart when max or min is removed. [...])
 
         """
-        # start = time.time()
 
+        # Remove old availability status
         if len(self.ups) == self.max_nb_data_points:
             was_up_timeframe_ago = self.ups.popleft()
         else:
             was_up_timeframe_ago = 0
 
+        # Add new availability status
         self.ups.append(is_up)
         self.successes_in_timeframe += is_up - was_up_timeframe_ago
 
         if was_up_timeframe_ago:
-            # remove old response time
+            # Remove old response time
             old_response_time = self.response_times.popleft()
             self.sum_response_times -= old_response_time
+            # Update response time stats
             if old_response_time == self.max_response_time:
                 self.max_response_time = max(self.response_times)
             if old_response_time == self.min_response_time:
                 self.min_response_time = min(self.response_times)
 
         if was_up_timeframe_ago or (always_a_response_code and len(self.response_codes) == self.max_nb_data_points):
-            # remove old response code
+            # Remove old response code
             old_response_code = self.response_codes.popleft()
+            # Update response code stats
             self.response_codes_dict[old_response_code] -= 1
 
         if is_up:
-            # add new response time
+            # Add new response time
             self.sum_response_times += response_time
             self.response_times.append(response_time)
+            # Update response time stats
             self.max_response_time = max(self.max_response_time, response_time)
             self.min_response_time = min(self.min_response_time, response_time)
 
         if is_up or always_a_response_code:
-            # add new response code
+            # Add new response code
             self.response_codes.append(response_code)
+            # Update response code stats
             if response_code in self.response_codes_dict:
                 self.response_codes_dict[response_code] += 1
             else:
                 self.response_codes_dict[response_code] = 1
 
+        # Update remaining stats
         self.availability = self.successes_in_timeframe / self.nb_data_points()
         if self.successes_in_timeframe > 0:
             self.average_response_time = self.sum_response_times / self.successes_in_timeframe
@@ -119,13 +127,22 @@ class Stats(object):
         assert len(self.response_times) == self.successes_in_timeframe
 
     def nb_data_points(self):
+        """Returns the number of data points recieved yet.
+
+        Slowly increases up to self.max_nb_data_points.
+        """
         return len(self.ups)
 
     def timeframe_reached(self):
+        """Return whether we have already reached the max number of data points."""
         return self.nb_data_points() == self.max_nb_data_points
 
 
 class HttpStats(Stats):
+    """Use to store stats for HTTP pings.
+
+    Ensures both response times and response codes are provided if the website is online
+    """
     def update(self, is_up, response_time=None, response_code=None):
         if is_up and (response_time is None or response_time is None):
             raise ValueError("Site is available but no additional information given")
@@ -134,8 +151,15 @@ class HttpStats(Stats):
 
 
 class PingStats(Stats):
+    """Use to store stats for ICMP pings.
+
+    Ensures a response code is provided, and
+    ensures a response time is provided if the website is online.
+    """
     def update(self, is_up, response_time=None, response_code=None):
         if response_code is None:
             raise ValueError("Ping should return a response code")
+        if is_up and response_time is None:
+            raise ValueError("Successful ping should return a response time")
         super().update(is_up, response_time, response_code, always_a_response_code=True)
         assert len(self.response_codes) == len(self.ups), (len(self.response_codes), len(self.ups))
