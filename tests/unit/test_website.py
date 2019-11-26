c85852426270823dbf75988f7b618280f4286d6e
Japillow <james@barrios.me>
2019-11-26 23:31:28 +0100

    add tests for http stats

--- tests/unit/test_website.py
+++ tests/unit/test_website.py
@@ -1,11 +1,12 @@
 import mock
 import pytest
 
+from stella import config
 from stella.website import Website
 
-TIMEFRAME_FOR_ALERTS = 120
+TIMEFRAME_FOR_ALERTS = config.ALERTING_TIMEFRAME
 SERVER_DOWNTIME = 3 * TIMEFRAME_FOR_ALERTS
-THRESHOLD = 0.8
+DEFAULT_ALERT_THRESHOLD = 0.8
 
 
 @pytest.fixture
@@ -29,11 +30,11 @@ class PingMock(object):
         return self.ping_down_n_then_up(SERVER_DOWNTIME)
 
 
-class TestAlerting(object):
-
+class TestHttpAlerting(object):
     @mock.patch('stella.website.Website.ping')
     def test_alerting_wait_for_enough_data(self, mock):
         """Website is always down since the beginning.
+
         An alert should only be sent when enough data has been gathered.
         """
         ping_mock = PingMock()
@@ -43,14 +44,17 @@ class TestAlerting(object):
 
         for i in range(TIMEFRAME_FOR_ALERTS - 1):
             website.ping_and_update_ping_stats()
-            website.check_for_alert()
+            website.check_http_stats_for_alert()
 
-        assert website.ping_stats_list[120].response_codes_dict == {999: TIMEFRAME_FOR_ALERTS - 1}
+        assert website.ping_stats_list[TIMEFRAME_FOR_ALERTS].response_codes_dict == {
+            999: TIMEFRAME_FOR_ALERTS - 1
+        }
         assert (len(website.alert_history) == 0), website.alert_history
 
     @mock.patch('stella.website.Website.ping')
     def test_alerting_fire_alert(self, mock):
         """Website is down since TIMEFRAME_FOR_ALERTS.
+
         An alert should be sent as enough data has been gathered.
         """
         ping_mock = PingMock()
@@ -59,14 +63,17 @@ class TestAlerting(object):
 
         for i in range(TIMEFRAME_FOR_ALERTS):
             website.ping_and_update_ping_stats()
-            website.check_for_alert()
-        assert website.ping_stats_list[120].response_codes_dict == {999: TIMEFRAME_FOR_ALERTS}
+            website.check_http_stats_for_alert()
+        assert website.ping_stats_list[TIMEFRAME_FOR_ALERTS].response_codes_dict == {
+            999: TIMEFRAME_FOR_ALERTS
+        }
 
         assert (len(website.alert_history) == 1), website.alert_history
 
     @mock.patch('stella.website.Website.ping')
     def test_alerting_only_one_alert_since_down(self, mock):
         """Website is still down.
+
         Only one alert should be triggered.
         """
         ping_mock = PingMock()
@@ -75,8 +82,10 @@ class TestAlerting(object):
 
         for i in range(SERVER_DOWNTIME):
             website.ping_and_update_ping_stats()
-            website.check_for_alert()
-        assert website.ping_stats_list[120].response_codes_dict == {999: TIMEFRAME_FOR_ALERTS}
+            website.check_http_stats_for_alert()
+        assert website.ping_stats_list[TIMEFRAME_FOR_ALERTS].response_codes_dict == {
+            999: TIMEFRAME_FOR_ALERTS
+        }
 
         # Only one firing alert
         assert (len(website.alert_history) == 1), website.alert_history
@@ -84,6 +93,7 @@ class TestAlerting(object):
     @mock.patch('stella.website.Website.ping')
     def test_alerting_wait_before_recovering(self, mock):
         """Website is back up after being down for a long time.
+
         An recovery alert shouldn't be triggered immediately when the website goes back up.
         """
         ping_mock = PingMock()
@@ -92,15 +102,18 @@ class TestAlerting(object):
 
         for i in range(SERVER_DOWNTIME + 1):
             website.ping_and_update_ping_stats()
-            website.check_for_alert()
-        assert website.ping_stats_list[120].response_codes_dict == {999: TIMEFRAME_FOR_ALERTS}
+            website.check_http_stats_for_alert()
+        assert website.ping_stats_list[TIMEFRAME_FOR_ALERTS].response_codes_dict == {
+            999: TIMEFRAME_FOR_ALERTS
+        }
 
         # Only the firing alert, no recovery alert
         assert (len(website.alert_history) == 1), website.alert_history
 
     @mock.patch('stella.website.Website.ping')
     def test_alerting_wait_for_threshold_before_recovering(self, mock):
-        """Website has been back up for a while
+        """Website has been back up for a while.
+
         Wait until availability is back up to the threshold to send the alert.
         """
         ping_mock = PingMock()
@@ -109,14 +122,18 @@ class TestAlerting(object):
 
         for i in range(SERVER_DOWNTIME + 2):
             website.ping_and_update_ping_stats()
-            website.check_for_alert()
-        assert website.ping_stats_list[120].response_codes_dict == {111: 1, 999: TIMEFRAME_FOR_ALERTS - 1}
+            website.check_http_stats_for_alert()
+        assert website.ping_stats_list[TIMEFRAME_FOR_ALERTS].response_codes_dict == {
+            111: 1,
+            999: TIMEFRAME_FOR_ALERTS - 1
+        }
 
         assert (len(website.alert_history) == 1), website.alert_history
 
     @mock.patch('stella.website.Website.ping')
     def test_alerting_should_have_recovered(self, mock):
-        """Website has been back up for a while
+        """Website has been back up for a while.
+
         Wait until availability is back up to the threshold to send the alert.
         """
         ping_mock = PingMock()
@@ -125,9 +142,129 @@ class TestAlerting(object):
 
         for i in range(SERVER_DOWNTIME + int(0.8 * TIMEFRAME_FOR_ALERTS)):
             website.ping_and_update_ping_stats()
-            website.check_for_alert()
+            website.check_http_stats_for_alert()
         assert (len(website.alert_history) == 1), website.alert_history
 
         website.ping_and_update_ping_stats()
-        website.check_for_alert()
+        website.check_http_stats_for_alert()
+        assert (len(website.alert_history) == 2), website.alert_history
+
+
+class TestPingAlerting(object):
+    @mock.patch('stella.website.Website.ping')
+    def test_alerting_wait_for_enough_data(self, mock):
+        """Website is always down since the beginning.
+
+        An alert should only be sent when enough data has been gathered.
+        """
+        ping_mock = PingMock()
+        mock.side_effect = ping_mock.ping_down_then_up
+
+        website = Website("fakehost.url", 1)
+
+        for i in range(TIMEFRAME_FOR_ALERTS - 1):
+            website.ping_and_update_ping_stats()
+            website.check_ping_stats_for_alert()
+
+        assert website.ping_stats_list[TIMEFRAME_FOR_ALERTS].response_codes_dict == {
+            999: TIMEFRAME_FOR_ALERTS - 1
+        }
+        assert (len(website.alert_history) == 0), website.alert_history
+
+    @mock.patch('stella.website.Website.ping')
+    def test_alerting_fire_alert(self, mock):
+        """Website is down since TIMEFRAME_FOR_ALERTS.
+
+        An alert should be sent as enough data has been gathered.
+        """
+        ping_mock = PingMock()
+        mock.side_effect = ping_mock.ping_down_then_up
+        website = Website("fakehost.url", 1)
+
+        for i in range(TIMEFRAME_FOR_ALERTS):
+            website.ping_and_update_ping_stats()
+            website.check_ping_stats_for_alert()
+        assert website.ping_stats_list[TIMEFRAME_FOR_ALERTS].response_codes_dict == {
+            999: TIMEFRAME_FOR_ALERTS
+        }
+
+        assert (len(website.alert_history) == 1), website.alert_history
+
+    @mock.patch('stella.website.Website.ping')
+    def test_alerting_only_one_alert_since_down(self, mock):
+        """Website is still down.
+
+        Only one alert should be triggered.
+        """
+        ping_mock = PingMock()
+        mock.side_effect = ping_mock.ping_down_then_up
+        website = Website("fakehost.url", 1)
+
+        for i in range(SERVER_DOWNTIME):
+            website.ping_and_update_ping_stats()
+            website.check_ping_stats_for_alert()
+        assert website.ping_stats_list[TIMEFRAME_FOR_ALERTS].response_codes_dict == {
+            999: TIMEFRAME_FOR_ALERTS
+        }
+
+        # Only one firing alert
+        assert (len(website.alert_history) == 1), website.alert_history
+
+    @mock.patch('stella.website.Website.ping')
+    def test_alerting_wait_before_recovering(self, mock):
+        """Website is back up after being down for a long time.
+
+        An recovery alert shouldn't be triggered immediately when the website goes back up.
+        """
+        ping_mock = PingMock()
+        mock.side_effect = ping_mock.ping_down_then_up
+        website = Website("fakehost.url", 1)
+
+        for i in range(SERVER_DOWNTIME + 1):
+            website.ping_and_update_ping_stats()
+            website.check_ping_stats_for_alert()
+        assert website.ping_stats_list[TIMEFRAME_FOR_ALERTS].response_codes_dict == {
+            999: TIMEFRAME_FOR_ALERTS
+        }
+
+        # Only the firing alert, no recovery alert
+        assert (len(website.alert_history) == 1), website.alert_history
+
+    @mock.patch('stella.website.Website.ping')
+    def test_alerting_wait_for_threshold_before_recovering(self, mock):
+        """Website has been back up for a while.
+
+        Wait until availability is back up to the threshold to send the alert.
+        """
+        ping_mock = PingMock()
+        mock.side_effect = ping_mock.ping_down_then_up
+        website = Website("fakehost.url", 1)
+
+        for i in range(SERVER_DOWNTIME + 2):
+            website.ping_and_update_ping_stats()
+            website.check_ping_stats_for_alert()
+        assert website.ping_stats_list[TIMEFRAME_FOR_ALERTS].response_codes_dict == {
+            111: 1,
+            999: TIMEFRAME_FOR_ALERTS - 1
+        }
+
+        assert (len(website.alert_history) == 1), website.alert_history
+
+    @mock.patch('stella.website.Website.ping')
+    def test_alerting_should_have_recovered(self, mock):
+        """Website has been back up for a while.
+
+        Wait until availability is back up to the threshold to send the alert.
+        """
+        ping_mock = PingMock()
+        mock.side_effect = ping_mock.ping_down_then_up
+        website = Website("fakehost.url", 1)
+
+        for i in range(SERVER_DOWNTIME + int(0.8 * TIMEFRAME_FOR_ALERTS)):
+            website.ping_and_update_ping_stats()
+            website.check_ping_stats_for_alert()
+        assert (len(website.alert_history) == 1), website.alert_history
+
+        website.ping_and_update_ping_stats()
+        website.check_ping_stats_for_alert()
         assert (len(website.alert_history) == 2), website.alert_history
