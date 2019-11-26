733d010663591c221d29f162a76dd5d9de91083b
Japillow <james@barrios.me>
2019-11-26 23:32:14 +0100

    add config used for screenshots

--- stella/config.py
+++ stella/config.py
@@ -4,6 +4,17 @@ hour = 60 * minute
 
 WEBSITES_FILE = "websites.conf"
 
+MONITOR_HTTP_RATHER_THAN_ICMP = True
+
+# # Used for README.md screenshots
+# # Stats
+# STATS_TIMEFRAMES = [30 * second, 2 * minute, 5 * minute, 10 * minute]
+
+# # Alerting
+# DEFAULT_ALERT_THRESHOLD = 0.8
+# ALERTING_TIMEFRAME = 5 * second
+
+# Suggested values
 # Stats
 STATS_TIMEFRAMES = [10 * minute, 1 * hour]
 
